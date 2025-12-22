d08cdab4136b5316fe249c9d3506b9e632807ad0
Smrutishree Misra <smrutishreemisra@gmail.com>
2025-12-22 06:01:21 +0000

    function

---
+++ function.js
@@ -0,0 +1,12 @@
+function sayHello(name) {
+    return `Hello, ${name}!`;
+}
+//reference = sayHello;
+//execution = sayHello("World");
+function add(a, b) {
+    return a + b;
+}
+add(2,3);
+add(5,"7");
+add(5,"a");
+
--- object.js
+++ object.js
@@ -1,21 +1,53 @@
-// creation of an object
-// the name should be capitalised
-const JsUser ={
-    name: "John",
-    "full name": "John Doe",
-    age: 30,
-    // for symbols
-    [mySym]: "myValue",
-    city: "New York",
-    email:"john@example.com",
-    isLoggedIn: true,
-    skills: ["JavaScript", "React", "Node.js"]
+// // creation of an object
+// // the name should be capitalised
+// const mySym = Symbol("mySym");
+
+// const JsUser ={
+//     name: "John",
+//     "full name": "John Doe",
+//     age: 30,
+//     // for symbols
+//     [mySym]: "myValue",
+//     city: "New York",
+//     email:"john@example.com",
+//     isLoggedIn: true,
+//     skills: ["JavaScript", "React", "Node.js"]
+// }
+
+// // console.log(JsUser["email"]);
+// // console.log(JsUser["full name"]);
+// // console.log(JsUser.email);
+// // console.log(JsUser["isLoggedIn"]);
+// // console.log(JsUser[mySym]);
+
+
+// // change vales in object
+// // JsUser.email ="john.doe@example.com";
+// // console.log(JsUser.email);
+
+// //freeze the object
+// // Object.freeze(JsUser);
+// // JsUser.email ="john.withfreezedoe@example.com";
+// // console.log(JsUser.email); // will not change
+
+// // console.log(JsUser);
+
+// JsUser.greeting = function(){
+//     console.log(`Hello, my name is ${this.name}`);
+// }
+
+// console.log(JsUser.greeting());
+
+// destructuring of object
+const company ={
+    name:"SM groupe of technologies",
+    location :"bhubaneswar",
+    established:2030,
+    ceo:"Mrs Smrutishree Mihir Misra",
+    employees:50000,
+    cfo:"Mr Mihir kumar behera"
+
 }
-
-console.log(JsUser["email"]);
-console.log(JsUser["full name"]);
-console.log(JsUser.email);
-console.log(JsUser["isLoggedIn"]);
-console.log(JsUser[mySym]);
-
+const {established : estd} = company;
+console.log(estd);
 
